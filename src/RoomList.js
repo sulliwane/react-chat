32b33d3981588d35b98a9f700b9f861f1262cccd
editrobot <2662908789@qq.com>
2016-01-15 16:02:41 +0800

    updata ul tag

--- src/RoomList.js
+++ src/RoomList.js
@@ -1,9 +1,21 @@
 import React, {Component} from 'react';
 
+var divStyle = {
+  color: 'white'
+};
+
 export default class RoomList extends Component {
     render() {
       return (
           <div id="RoomListBox">
+          <ul>
+          <li>1</li>
+          <li>2</li>
+          <li>3</li>
+          <li>4</li>
+          <li>5</li>
+          <li>6</li>
+          <ul>
           </div>
       );
     }
