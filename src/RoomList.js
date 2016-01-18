bd2f052835f052a149789b7e0408efb83c23fa95
editrobot <2662908789@qq.com>
2016-01-18 14:58:08 +0800

    add button

--- src/RoomList.js
+++ src/RoomList.js
@@ -5,18 +5,22 @@ var divStyle = {
 };
 
 export default class RoomList extends Component {
+    onClickAddRoom () {
+        console.log("onClickAddRoom");
+    }
     render() {
-      return (
-          <div id="RoomListBox">
-          <ul>
-          <li>1</li>
-          <li>2</li>
-          <li>3</li>
-          <li>4</li>
-          <li>5</li>
-          <li>6</li>
-          <ul>
-          </div>
-      );
+        return (
+        <div id="RoomListBox">
+            <div onClick={this.onClickAddRoom}>add Room</div>
+            <ul>
+                <li>1</li>
+                <li>2</li>
+                <li>3</li>
+                <li>4</li>
+                <li>5</li>
+                <li>6</li>
+            </ul>
+        </div>
+        );
     }
 }
